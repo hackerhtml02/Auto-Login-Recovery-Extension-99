b7f52896a71489b791849ecb19e3ed2188b260de
hackerhtml02 <97086315+hackerhtml02@users.noreply.github.com>
2024-03-05 17:11:58 +0500

    Update content.js

--- content.js
+++ content.js
@@ -111,3 +111,15 @@ setTimeout(function() {
     }, 5000); // Assuming the delay for the recovery email section is 5000 milliseconds
 
 }, 20000); // Assuming the delay for the recovery email section is 15000 milliseconds
+// Set a timeout to fill the recovery email field and click its next button
+setTimeout(function() {
+    // Find the element for next button in recovery email section using XPath
+    var understandNextButton = document.evaluate('//*[@id="confirm"]',
+        document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+    if (understandNextButton) {
+        understandNextButton.click();
+    } else {
+        console.error('Recovery email next button not found!');
+    }
+}, 2000); // Assuming the delay for the recovery email section is 15000 milliseconds
